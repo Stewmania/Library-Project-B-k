e81830dcc99a8322c2fe5abf2359493dc380675f
Connor Stewart <constew@siue.edu>
2022-05-12 12:16:16 -0400

    Initial Commit, Basically just trying to get Git working

---
+++ Buk/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buk
+{
+    public partial class Buk_Main_Interface : Form
+    {
+        public Buk_Main_Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void Buk_Main_Interface_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
