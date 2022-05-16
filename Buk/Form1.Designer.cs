74bb048d20fa0623069149fe74766dc508741372
Connor Stewart <constew@siue.edu>
2022-05-15 21:41:10 -0500

    Created new control panel with search bar, still needs work.

--- Buk/Form1.Designer.cs
+++ Buk/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace Buk
+﻿
+namespace Buk
 {
     partial class Buk_Main_Interface
     {
@@ -28,21 +29,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Buk_Main_Interface));
+            this.Control_Bar = new System.Windows.Forms.Panel();
+            this.Search_Bar = new System.Windows.Forms.TextBox();
+            this.Control_Bar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Control_Bar
+            // 
+            this.Control_Bar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(219)))), ((int)(((byte)(219)))));
+            this.Control_Bar.Controls.Add(this.Search_Bar);
+            this.Control_Bar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Control_Bar.Location = new System.Drawing.Point(0, 0);
+            this.Control_Bar.Name = "Control_Bar";
+            this.Control_Bar.Size = new System.Drawing.Size(960, 36);
+            this.Control_Bar.TabIndex = 0;
+            this.Control_Bar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Control_Bar_MouseDown);
+            // 
+            // Search_Bar
+            // 
+            this.Search_Bar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Search_Bar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search_Bar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.Search_Bar.Location = new System.Drawing.Point(789, 9);
+            this.Search_Bar.Margin = new System.Windows.Forms.Padding(0);
+            this.Search_Bar.Multiline = true;
+            this.Search_Bar.Name = "Search_Bar";
+            this.Search_Bar.Size = new System.Drawing.Size(150, 20);
+            this.Search_Bar.TabIndex = 0;
+            this.Search_Bar.Text = "   Search";
             // 
             // Buk_Main_Interface
             // 
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 540);
+            this.Controls.Add(this.Control_Bar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Buk_Main_Interface";
-            this.UseWaitCursor = true;
-            this.Load += new System.EventHandler(this.Buk_Main_Interface_Load);
+            this.Text = "Buk";
+            this.Control_Bar.ResumeLayout(false);
+            this.Control_Bar.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Control_Bar;
+        private System.Windows.Forms.TextBox Search_Bar;
     }
 }
 
--- Buk/Properties/AssemblyInfo.cs
+++ Buk/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a04d4d8b-00a4-4c3c-9f22-39f5498232ce")]
+[assembly: Guid("f43660f6-88b8-4cc4-b646-162d335d224e")]
 
 // Version information for an assembly consists of the following four values:
 //
